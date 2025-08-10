70b8a9f5c045ac32e03a4683e1d387574e679464
Maarten Behn <maarten.behn@gmail.com>
2025-08-11 00:35:09 +0200

    removing allocated vec and worker vec

--- src/multi_data_buffer/mod.rs
+++ src/multi_data_buffer/mod.rs
@@ -1,6 +1,10 @@
+use octa_force::OctaResult;
+
 pub mod buddy_buffer_allocator;
-pub mod allocated_vec;
 pub mod cached_vec;
 pub mod parallel_vec;
-pub mod worker_vec;
+
+pub trait DataBuffer<T> {
+    fn push(&self, values: &[T]) -> OctaResult<u32>;
+}
 
